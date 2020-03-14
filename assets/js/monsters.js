958204e659b3d7cbeabdf7e55092564a943af79f
Aoki <youlikehitsyo2@gmail.com>
2020-03-15 00:32:01 +0100

    update

---
+++ assets/js/monsters.js
@@ -0,0 +1,142 @@
+const monsters = [
+  {
+    name: 'Gargoyle',
+    awakenName: 'Kunite',
+    element: 'Fire',
+    stars: 4,
+    image: 'assets/img/monsters/kunite.png'
+  },
+  {
+    name: 'Gargoyle',
+    awakenName: 'Tanzaite',
+    element: 'Water',
+    stars: 4,
+    image: 'assets/img/monsters/tanzaite.png'
+  },
+  {
+    name: 'Gargoyle',
+    awakenName: 'Malite',
+    element: 'Wind',
+    stars: 4,
+    image: 'assets/img/monsters/malite.png'
+  },
+  {
+    name: 'Gargoyle',
+    awakenName: 'Phenaka',
+    element: 'Light',
+    stars: 4,
+    image: 'assets/img/monsters/phenaka.png'
+  },
+  {
+    name: 'Gargoyle',
+    awakenName: 'Onyx',
+    element: 'Dark',
+    stars: 4,
+    image: 'assets/img/monsters/onyx.png'
+  },
+  {
+    name: 'Panda Warrior',
+    awakenName: 'Mo Long',
+    element: 'Water',
+    stars: 5,
+    image: 'assets/img/monsters/mo_long.png'
+  },
+  {
+    name: 'Panda Warrior',
+    awakenName: 'Feng Yan',
+    element: 'Wind',
+    stars: 5,
+    image: 'assets/img/monsters/feng_yan.png'
+  },
+  {
+    name: 'Panda Warrior',
+    awakenName: 'Xiong Fei',
+    element: 'Fire',
+    stars: 5,
+    image: 'assets/img/monsters/xiong_fei.png'
+  },
+  {
+    name: 'Panda Warrior',
+    awakenName: 'Tian Lang',
+    element: 'Light',
+    stars: 5,
+    image: 'assets/img/monsters/tian_lang.png'
+  },
+  {
+    name: 'Panda Warrior',
+    awakenName: 'Mi Ying',
+    element: 'Dark',
+    stars: 5,
+    image: 'assets/img/monsters/mi_ying.png'
+  },
+  {
+    name: 'Demon',
+    awakenName: 'Bael',
+    element: 'Fire',
+    stars: 5,
+    image: 'assets/img/monsters/bael.png'
+  },
+  {
+    name: 'Demon',
+    awakenName: 'Belial',
+    element: 'Water',
+    stars: 5,
+    image: 'assets/img/monsters/belial.png'
+  },
+  {
+    name: 'Demon',
+    awakenName: 'Mephisto',
+    element: 'Wind',
+    stars: 5,
+    image: 'assets/img/monsters/mephisto.png'
+  },
+  {
+    name: 'Demon',
+    awakenName: 'Lucifer',
+    element: 'Light',
+    stars: 5,
+    image: 'assets/img/monsters/lucifer.png'
+  },
+  {
+    name: 'Demon',
+    awakenName: 'Beelzebub',
+    element: 'Dark',
+    stars: 5,
+    image: 'assets/img/monsters/beelzebub.png'
+  },
+  {
+    name: 'Hell Lady',
+    awakenName: 'Raki',
+    element: 'Fire',
+    stars: 5,
+    image: 'assets/img/monsters/raki.png'
+  },
+  {
+    name: 'Hell Lady',
+    awakenName: 'Beth',
+    element: 'Water',
+    stars: 5,
+    image: 'assets/img/monsters/beth.png'
+  },
+  {
+    name: 'Hell Lady',
+    awakenName: 'Ethna',
+    element: 'Wind',
+    stars: 5,
+    image: 'assets/img/monsters/ethna.png'
+  },
+  {
+    name: 'Hell Lady',
+    awakenName: 'Asima',
+    element: 'Light',
+    stars: 5,
+    image: 'assets/img/monsters/asima.png'
+  },
+  {
+    name: 'Hell Lady',
+    awakenName: 'Craka',
+    element: 'Dark',
+    stars: 5,
+    image: 'assets/img/monsters/craka.png'
+  }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,101 @@
-// Array of objects with monsters
-const monsters = [
-  {
-    name: 'Cow Girl',
-    awakenName: 'Loren',
-    element: 'Light',
-    stars: 3,
-    image: 'assets/img/monsters/loren.png'
-  },
-  {
-    name: 'Gargoyle',
-    awakenName: 'Malite',
-    element: 'Wind',
-    stars: 4,
-    image: 'assets/img/monsters/malite.png'
-  },
-  {
-    name: 'Hell Lady',
-    awakenName: 'Beth',
-    element: 'Water',
-    stars: 5,
-    image: 'assets/img/monsters/beth.png'
-  },
-  {
-    name: 'Beast Monk',
-    awakenName: 'Rahul',
-    element: 'Dark',
-    stars: 5,
-    image: 'assets/img/monsters/rahul.png'
-  },
-  {
-    name: 'Panda Warrior',
-    awakenName: 'Feng Yan',
-    element: 'Wind',
-    stars: 5,
-    image: 'assets/img/monsters/feng_yan.png'
-  }
-]
+const scrollMystical = monsters.filter(function(x) {
+  return ['Fire', 'Wind', 'Water'].includes(x.element);
+});
+
+const scrollFire = monsters.filter(function(x) {
+  return x.element === "Fire";
+});
+
+const scrollWater = monsters.filter(function(x) {
+  return x.element === "Water";
+});
+
+const scrollWind = monsters.filter(function(x) {
+  return x.element === "Wind";
+});
+
+const scrollLightDark = monsters.filter(function(x) {
+  return ['Light', 'Dark'].includes(x.element);
+});
+
+const scrollLegendary = monsters.filter(function(x) {
+  return [4, 5].includes(x.stars) && ['Fire', 'Water', 'Wind'].includes(x.element);
+});
 
 // Total amount of summons
 let statsTotal = 0;
+let statsThreeStar = 0;
+let statsFourStar = 0;
+let statsFiveStar = 0;
+let statsFire = 0;
+let statsWater = 0;
+let statsWind = 0;
+let statsLight = 0;
+let statsDark = 0;
+
+// Latest summons
+let latestSummons = [];
+
+// Select scroll
+const scrollList = document.getElementById('scrolls');
+const scrollType = document.getElementsByTagName('li');
+
+for (let i = 0; i < scrollType.length; i++) {
+  scrollType[i].addEventListener('click', function() {
+    let current = document.getElementsByClassName('active');
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(' active', '');
+    }
+
+    this.className += ' active';
+  })
+};
 
 // What happens when you click the summon button
+
+document.getElementById('summon').addEventListener('click', function() {
+  if (document.getElementById('legendary').classList.contains('active')) {
+
+    let summonLegendary = scrollLegendary[Math.floor(Math.random() * scrollLegendary.length)];
+    console.log(summonLegendary.awakenName);
+
+  } else if (document.getElementById('ld').classList.contains('active')) {
+    console.log('ld');
+  } else if (document.getElementById('mystical').classList.contains('active')) {
+
+    let random = Math.random() * 915;
+    if (random <= 5) {
+
+      // 5 star
+
+    } else if (random <= 80) {
+
+      // 4 star
+
+    } else if (random <= 915) {
+
+      // 3 star
+
+    } else {
+      console.log('error');
+    }
+
+  } else if (document.getElementById('fire').classList.contains('active')) {
+    console.log('fire');
+  } else if (document.getElementById('water').classList.contains('active')) {
+    console.log('water');
+  } else if (document.getElementById('wind').classList.contains('active')) {
+    console.log('wind');
+  } else {
+    alert('Select a scroll before summoning!');
+  }
+});
+
+let summonMystical = scrollMystical[Math.floor(Math.random() * scrollMystical.length)];
+console.log(summonMystical);
+
+/*
 document.getElementById('summon').addEventListener('click', function() {
   document.getElementById('loading-summon').style.display = 'block'; // Display the loading image
   document.getElementById('monster-summon').style.display = 'none'; // Hide the monster name so it doesn't overlap the loading image
@@ -48,13 +103,6 @@ document.getElementById('summon').addEventListener('click', function() {
   setTimeout(function() {
     document.getElementById('loading-summon').style.display = 'none'; // Hide the loading image
     document.getElementById('monster-summon').style.display = 'block'; // Display the monster name again since we hid it
-
-    // Update summon counter
-    updateStats();
-    function updateStats() {
-      statsTotal++;
-      document.getElementById('stats-total').innerText = statsTotal;
-    }
 
     // Get a random monster from the object array
     let randomMonster = monsters[Math.floor(Math.random() * monsters.length)];
@@ -93,5 +141,61 @@ document.getElementById('summon').addEventListener('click', function() {
     let randomMonsterImage = randomMonster.image;
     document.getElementById('monster-image').src=randomMonsterImage;
 
+    // Update Stats
+    function updateStats() {
+      // Update total stats
+      statsTotal++;
+      document.getElementById('stats-total').innerText = statsTotal;
+
+      // Update element stats
+      let statsElement = randomMonster.element;
+      switch(statsElement) {
+        case 'Fire':
+          statsFire++;
+          document.getElementById('stats-fire').innerText = statsFire;
+          break;
+        case 'Water':
+          statsWater++;
+          document.getElementById('stats-water').innerText = statsWater;
+          break;
+        case 'Wind':
+          statsWind++;
+          document.getElementById('stats-wind').innerText = statsWind;
+          break;
+        case 'Light':
+          statsLight++;
+          document.getElementById('stats-light').innerText = statsLight;
+          break;
+        case 'Dark':
+          statsDark++;
+          document.getElementById('stats-dark').innerText = statsDark;
+          break;
+      }
+
+      // Update star stats
+      let statsStars = randomMonster.stars;
+      switch(statsStars) {
+        case 3:
+          statsThreeStar++;
+          document.getElementById('stats-threestar').innerText = statsThreeStar;
+          break;
+        case 4:
+          statsFourStar++;
+          document.getElementById('stats-fourstar').innerText = statsFourStar;
+          break;
+        case 5:
+          statsFiveStar++;
+          document.getElementById('stats-fivestar').innerText = statsFiveStar;
+          break;
+      }
+    }
+
+    // Update summon counter
+    updateStats();
+
+    // Display latest summoned monsters
+    latestSummons.push(randomMonster);
+
   }, 500)
 });
+*/
